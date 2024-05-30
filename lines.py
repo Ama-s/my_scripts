706516481e9b642ffe6947cc73748d23c7a80d99
Deborah Arku <arkudeborah123@gmail.com>
2024-05-30 11:01:25 +0100

    Update lines.py

--- lines.py
+++ lines.py
@@ -6,22 +6,22 @@ def main():
         if check_if_python_file(filename):
             total_lines = count_lines(filename)
             print(total_lines)
-            
+
 
 def argument_count():
     if len(sys.argv) < 2:
         print("Too few command-line arguments")
-        sys.exit()
+        sys.exit(1)
     elif len(sys.argv) > 2:
         print("Too many command-line arguments")
-        sys.exit()
+        sys.exit(1)
     else:
         return True
 
 def check_if_python_file(filename):
     if not filename.endswith(".py"):
         print("Not a Python file")
-        sys.exit()
+        sys.exit(1)
     else:
         return True
 
@@ -31,12 +31,12 @@ def count_lines(filename):
         with open(filename) as file:
             for line in file:
                 every_line = line.lstrip()
-                if every_line and not every_line.startswith("#"): 
+                if every_line and not every_line.startswith("#"):
                     # checks if it's a non-empty line that isn't a comment
                     count += 1
     except FileNotFoundError:
         print("File does not exist")
-        sys.exit()
+        sys.exit(1)
     return count
 
 
