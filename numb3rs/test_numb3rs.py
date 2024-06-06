2ae1bdebbbb2c752b1369fb507bd58891d83f233
Deborah Arku <arkudeborah123@gmail.com>
2024-06-06 20:18:32 +0100

    Update test_numb3rs.py

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -10,8 +10,9 @@ def test_invalid_ip():
     assert validate("192.168.0.1") == True
     assert validate("200.256.0.1") == False
     assert validate("275.0.1.25") == False
-    assert validate("25.33.11.23") == False
+    assert validate("25.33.11.23") == True
     assert validate("255.255.255.255") == True
+    assert validate("256.0.300.1") == False
 
 def no_nondigits():
     assert validate("cat.dog.mouse.food") == False
